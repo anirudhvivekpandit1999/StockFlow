d89a7ae382498fde6edd51e460b31c6a45aecc2b
anirudhp@technocrafts.co.in <anirudhp@technocrafts.co.in>
2025-06-11 15:17:20 +0530

    changes

--- src/services/globals.js
+++ src/services/globals.js
@@ -1,4 +1,4 @@
-export const url = 'https://e5eb-27-107-162-138.ngrok-free.app/api/';
+export const url = 'http://192.168.10.31:789/api/';
 export const stockInAndOutInboundOutboundMovementController = 'StockInAndOutInboundOutboundMovement/';
 export const addNewFormEndpoint = 'AddNewForm';
 export const getStockFlowDataEndpoint = 'GetStockFlowData';
--- src/services/syncService.js
+++ src/services/syncService.js
@@ -2,7 +2,7 @@ import NetInfo from '@react-native-community/netinfo';
 import { getUnsyncedInventory, markInventoryAsSynced, insertInventory } from './database';
 import axios from 'axios';
 
-const API_BASE_URL = 'https://2bd7-27-107-162-138.ngrok-free.app/api'; // Change to your .NET API base URL
+const API_BASE_URL = 'http://192.168.10.31:789/api'; // Change to your .NET API base URL
 
 export const syncInventory = async () => {
   const state = await NetInfo.fetch();
